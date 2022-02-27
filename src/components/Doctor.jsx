f9f07167e08389bfb15a1f0e815d5f339f704126
lostchristmas0 <chlost124@gmail.com>
2022-02-26 23:11:53 -0500

    update sort

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -5,15 +5,14 @@ import {Link} from 'react-router-dom'
 function Doctor({item}) {
   let dpt = item.department
 
+  // find all doctors within the same department who has the same specialty with current doctor (specialty has overlap)
   const findSimilar = (entry) => {
     return entry.id !== item.id && entry['department'] === dpt && entry['specialty'].filter((x) => (item.specialty.indexOf(x) > -1)).length > 0
   }
   let similar = DoctorData.filter(findSimilar)
 
-  const sortByReview = (list) => {
-    return list.review
-  }
-  similar.sort(sortByReview)
+  // similar doctors are sorted by their review score
+  similar.sort((a, b) => (b.review - a.review))
   // console.log(similar)
 
   return (
